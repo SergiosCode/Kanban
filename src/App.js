f088ff033e281996042a30084b60d13712a13589
SergioV <sergioscode@gmail.com>
2022-11-05 11:05:52 -0700

    Added nav items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import { ReactComponent as IconBoard } from "./assets/icon-board.svg";
 
 function App() {
   return (
     <div className="App">
-     
+      <nav>
+        <h1>kanban</h1>
+
+        <h4>{`All Boards (3)`}</h4>
+        <h3>
+          <IconBoard /> Platform Launch
+        </h3>
+        <h3>
+          <IconBoard /> Marketing Plan
+        </h3>
+        <h3>
+          <IconBoard /> Roadmap
+        </h3>
+        <h3>+ Create New Board</h3>
+      </nav>
     </div>
   );
 }
